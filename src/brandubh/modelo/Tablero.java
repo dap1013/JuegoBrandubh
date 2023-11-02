d2d8d8320720370ee9e38e654b2338606369fe9e
USUARIO <USUARIO@10.160.34.68>
2023-11-02 16:42:23 +0100

    2

--- src/brandubh/modelo/Tablero.java
+++ src/brandubh/modelo/Tablero.java
@@ -1,8 +1,14 @@
 package brandubh.modelo;
 
 /**
- * The Class Tablero.
+ * Clase del tipo Tablero.
+ * 
+ * @author Diego Arbeloa
+ * @author Victor Vidal
+ * @version 1.0
+ * @since 1.0
  */
+
 public class Tablero {
 
 }
