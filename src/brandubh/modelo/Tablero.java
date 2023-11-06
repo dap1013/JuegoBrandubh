9f1006de15ab653d77e0c6f9956f5f374e1ada8f
arbel <arbel@Ord_de_Diego>
2023-11-06 21:06:48 +0100

    Añadir signatura de métodos y métodos para inicializar tablero

--- src/brandubh/modelo/Tablero.java
+++ src/brandubh/modelo/Tablero.java
@@ -2,6 +2,7 @@ package brandubh.modelo;
 
 import brandubh.util.Coordenada;
 import brandubh.util.TipoCelda;
+import brandubh.util.TipoPieza;
 
 /**
  * Clase del tipo Tablero.
@@ -13,24 +14,102 @@ import brandubh.util.TipoCelda;
  */
 
 public class Tablero {
-
-	/** La pieza. */
-	public Pieza pieza;
+	
+	public static final int filas = 7;
+	public static final int columnas = 7;
 
 	private Celda[][] matriz;
 	
-	/** */
-	public Tablero(int filas, int columnas) {
-	matriz = new Celda[7][7];
-	for(int i = 0; i<matriz.length; i++) {
-		for(int j = 0; j<matriz[i].length; j++ ) {
-		Coordenada coordenada = new Coordenada (i, j);
-		Celda celda = new Celda(coordenada,TipoCelda.NORMAL);
-		matriz [i][j] = celda;
-		}
-	/*Usar el de Celda de la línea 44*/
-	matriz = new Celda[0][0];
+	public Tablero() {
+		matriz = new Celda[filas][columnas];
+		inicializarTablero();
 	}
 	
+	
+	private void inicializarTablero() {
+		for(int i = 0; i < filas; i++) {
+			for(int j = 0; j < columnas; j++) {
+				if((i == 0 && j == 0) || (i == 0 && j==6) || (i == 6 && j == 0) || i == 6 && j == 6) {
+					matriz[i][j] = new Celda(new Coordenada(i, j), TipoCelda.PROVINCIA);
+				}else if(i == 3 && j == 3) {
+					matriz[i][j] = new Celda(new Coordenada(i, j), TipoCelda.TRONO);
+				}else {
+					matriz[i][j] = new Celda(new Coordenada(i, j));
+				}
+			}
+		}
+		colocarPiezasInicio();
+		
 	}
+	private void colocarPiezasInicio() {
+		matriz[0][3].colocar(new Pieza(TipoPieza.ATACANTE));
+		matriz[1][3].colocar(new Pieza(TipoPieza.ATACANTE));
+		matriz[2][3].colocar(new Pieza(TipoPieza.DEFENSOR));
+		matriz[3][3].colocar(new Pieza(TipoPieza.REY));
+		matriz[4][3].colocar(new Pieza(TipoPieza.DEFENSOR));
+		matriz[5][3].colocar(new Pieza(TipoPieza.ATACANTE));
+		matriz[6][3].colocar(new Pieza(TipoPieza.ATACANTE));
+		matriz[3][0].colocar(new Pieza(TipoPieza.ATACANTE));
+		matriz[3][1].colocar(new Pieza(TipoPieza.ATACANTE));
+		matriz[3][2].colocar(new Pieza(TipoPieza.DEFENSOR));
+		matriz[3][4].colocar(new Pieza(TipoPieza.DEFENSOR));
+		matriz[3][5].colocar(new Pieza(TipoPieza.ATACANTE));
+		matriz[3][6].colocar(new Pieza(TipoPieza.ATACANTE));
+		
+	}
+	
+	public String aTexto() {
+		
+	}
+	
+	public Tablero clonar() {
+		
+	}
+	
+	public void colocar(Pieza pieza, Coordenada coordenada) {
+		
+	}
+	
+	public Celda consultarCelda(Coordenada coordenada) {
+		
+	}
+	
+	public Celda[] consultarCeldas() {
+		
+	}
+	
+	public Celda[] consultarCeldasContiguas(Coordenada coordenada) {
+		
+	}
+	
+	public Celda[] consultarCeldasContiguasEnHorizontal(Coordenada coordenada) {
+		
+	}
+	
+	public Celda[] consultarCeldasContiguasEnVertical(Coordenada coordenada) {
+		
+	}
+	
+	public int consultarNumeroColumnas() {
+		
+	}
+	
+	public int consultarNumeroFilas() {
+		
+	}
+	
+	public int consultarNumeroPiezas(TipoPieza tipoPieza) {
+		
+	}
+	
+	public void eliminarPieza(Coordenada coordenada) {
+		
+	}
+	public Celda obtenerCelda(Coordenada coordenada) {
+		
+	}
+	
+	//Falta por implementar toString(), hashCode() y equals(), generarlos cuando se termine la clase
+	
+	
 }
